4549bbd386f6e2db62f0befebc5bcccd309c98c3
Godwin <dministry356@gmail.com>
2023-02-16 03:35:51 -0800

    quoye

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main block
- * Return: 1
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	 , sizeof(phrase));
 	return (1);
 }
